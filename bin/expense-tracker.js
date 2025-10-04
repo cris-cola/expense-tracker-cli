50e9a72925dc733d3f1bee97fa77be05ba02ca87
Christian Colacillo <4365550+cris-cola@users.noreply.github.com>
2025-10-04 22:27:44 +0200

    chore: fix permission issue with wrapper

---
+++ bin/expense-tracker.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+// wrapper script that delegates to compiled TypeScript output in dist
+// This file should be committed with the executable bit set (git mode 100755)
+require('../dist/index.js');
