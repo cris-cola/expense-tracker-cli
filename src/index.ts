f61596e6665f5be07625054fd2747772ebc4a24e
Christian Colacillo <4365550+cris-cola@users.noreply.github.com>
2025-10-05 16:48:02 +0200

    feat: add command

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,100 @@
 #! /usr/bin/env node
 // ^ That “shebang” line makes it runnable as an executable if you chmod +x it.
-console.log("Hello from Expenses Planner CLI!");
 
 // initStore();
 
 import { Command } from "commander";
+import numbro from "numbro";
 
+function parseMoney(value: string): number {
+  const parsedAmount = numbro.unformat(value);
+  if (!Number.isFinite(parsedAmount)) {
+    throw new Error(`Invalid number value: ${value}`);
+  }
+  return parsedAmount;
+}
 
 export function validateCommand(argv: string[]) {
-
   const program = new Command();
 
   program
-    .name('task-cli')
+    .name('expense-tracker-cli')
     .description('CLI task tracker')
     .version('1.0f.0');
 
-  // program
-  //   .command('add <description>')
-  //   .description('Add a new task')
-  //   .action((description) => {
-  //     addTask(description);
-  //   });
+  // expense-tracker add --description "Lunch" --amount 20
+  program
+    .command('add')
+    .description('Add a new task')
+    .option("--description", "task description")
+    .argument("<string>")
+    .option("--amount")
+    .argument('<amount>', 'integer argument', parseMoney, 1000)
+    .action((description: string, amount: number) => {
+      console.log(`Add: ${description} — ${amount.toFixed(2)}`);
+    });
 
-  // program
-  //   .command('delete <id>')
-  //   .description('Delete a task')
-  //   .action((id) => {
-  //     deleteTask(parseInt(id));
-  //   });
+    /* program
+      .command('add')
+      .argument('<first>', 'integer argument', myParseInt)
+      .option("--second")
+      .argument('[second]', 'integer argument', myParseInt, 1000)
+      .action((first, second) => {
+        console.log(`${first} + ${second} = ${first + second}`);
+      })
+    ; */
 
-  // program
-  //   .command('list [status]')
-  //   .description('List tasks (optional: todo, in-progress, done)')
-  //   .action((status) => {
-  //     listTasks(status);
-  //   });
+    program
+      .command('clone <source> [destination]')
+      .description('clone a repository into a newly created directory')
+      .action((source, destination) => {
+        console.log('clone command called');
+      });
 
-  // program
-  //   .command('update <id> <description>')
-  //   .description('Update task description')
-  //   .action((id, description) => {
-  //     updateTask(parseInt(id), description);
-  //   });
+    program.command('split')
+      .description('Split a string into substrings and display as an array')
+      .argument('<string>', 'string to split')
+      .option('--first', 'display just the first substring')
+      .option('-s, --separatodr <chadr>', 'separator character', ',')
+      .action((str, options) => {
+        const limit = options.first ? 1 : undefined;
+       
+        console.log(str.split(options.separatodr, limit));
+      });
 
-  // program
-  //   .command('mark-done <id>')
-  //   .action((id) => {
-  //     markTask(parseInt(id), 'done');
-  //   });
+/* 
+  program
+    .command('delete <id>')
+    .description('Delete a task')
+    .action((id) => {
+      deleteTask(parseInt(id));
+    });
 
-  // program
-  //   .command('mark-in-progress <id>')
-  //   .action((id) => {
-  //     markTask(parseInt(id), 'in-progress');
-  //   });
+  program
+    .command('list [status]')
+    .description('List tasks (optional: todo, in-progress, done)')
+    .action((status) => {
+      listTasks(status);
+    });
+
+  program
+    .command('update <id> <description>')
+    .description('Update task description')
+    .action((id, description) => {
+      updateTask(parseInt(id), description);
+    });
+
+  program
+    .command('mark-done <id>')
+    .action((id) => {
+      markTask(parseInt(id), 'done');
+    });
+
+  program
+    .command('mark-in-progress <id>')
+    .action((id) => {
+      markTask(parseInt(id), 'in-progress');
+    }); */
 
   program.parse();
 }
@@ -71,22 +111,5 @@ class App {
   }
 }
 
-function addTask(description: any) {
-  console.log("addTask");
-}
-function deleteTask(arg0: number) {
-  throw new Error("Function not implemented.");
-}
-
-function listTasks(status: any) {
-  throw new Error("Function not implemented.");
-}
-
-function updateTask(arg0: number, description: any) {
-  throw new Error("Function not implemented.");
-}
-
-function markTask(arg0: number, arg1: string) {
-  throw new Error("Function not implemented.");
-}
+App.start();
 
